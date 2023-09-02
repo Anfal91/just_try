6c36be8d8af92f4db13f5fab22c4c2a5f16b10c1
anfal-adroapps <109583481+anfal-adroapps@users.noreply.github.com>
2023-09-02 22:50:00 +0530

    cardUpdates

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+let planetCard = document.getElementById('planetCard');
+let cardCloseBtn = document.getElementById('cardCloseBtn');
+
+cardCloseBtn.onclick = () =>{
+    planetCard.style.display = 'none';
+}
